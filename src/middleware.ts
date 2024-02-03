a9f73d871b640edd91ab49ecd1c616fd51afdc40
hyeonjun-L <beeminor60@gmail.com>
2024-02-03 23:08:05 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,123 +17,34 @@ const setCookie = (response: NextResponse, name: string, value: string) => {
   });
 };
 
+const checkAccessToken = (headers: Record<string, string>, point: string) => {
+  return fetch(new URL(`${END_POINT}/auth/token/verify/${point}`).href, {
+    method: 'GET',
+    credentials: 'include',
+    headers,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};
+
 export const middleware = (request: NextRequest) => {
   const user = request.cookies.get('userAccessToken')?.value;
   const lecturer = request.cookies.get('lecturerAccessToken')?.value;
-  let response: NextResponse<unknown> | null = null;
+  let clientResponse: NextResponse<unknown> | null = null;
 
   if (user || lecturer) {
     const point = user ? 'user-access-token' : 'lecturer-access-token';
 
     const headers: Record<string, string> = {
       Authorization: `Bearer ${user || lecturer}`,
+      'Accept-Encoding': 'zlib',
     };
 
-    return fetch(new URL(`${END_POINT}/auth/token/verify/${point}`).href, {
-      method: 'GET',
-      credentials: 'include',
-      headers,
-    })
-      .then((response) => {
-        console.log('hi');
-
-        response.json().then((data) => {
-          console.log(data);
-          if (user && USER_NO_ACCESS.includes(request.nextUrl.pathname)) {
-            // 유저가 가면 안되는 lecturer 링크
-            console.log('user');
-            response = NextResponse.redirect(new URL('/', request.url));
-          } else if (
-            lecturer &&
-            LECTURER_NO_ACCESS.includes(request.nextUrl.pathname)
-          ) {
-            // 강사가 가면 안되는 user 링크 확인
-            response = NextResponse.redirect(new URL('/', request.url));
-          } else if (
-            NON_ACCESSIBLE_AFTER_LOGIN.includes(request.nextUrl.pathname)
-          ) {
-            //로그인해서 가면 안되는 링크
-            response = NextResponse.redirect(new URL('/', request.url));
-          } else {
-            console.log('next');
-
-            response = NextResponse.next();
-          }
-        });
-      })
-      .catch((error) => {
-        if (error.status === 401) {
-          const refreshToken = request.cookies.get('refreshToken')?.value;
-
-          return fetch(new URL(`${END_POINT}/auth/token/refresh`).href, {
-            credentials: 'include',
-            headers: {
-              'Content-Type': 'application/json',
-              Cookie: `refreshToken=${refreshToken}`,
-            },
-          })
-            .then((response) => {
-              if (!response.ok) {
-                throw response;
-              }
-
-              const resCookies = response.headers
-                .get('set-cookie')
-                ?.split('; ');
-              const refreshTokenCookie = resCookies?.find((cookie) =>
-                cookie.startsWith('refreshToken='),
-              );
-              const resRefreshToken = refreshTokenCookie?.split('=')[1];
-
-              if (!resRefreshToken) {
-                throw new Error('refresh token 재발급 에러');
-              }
-
-              return response.json().then((json) => {
-                const userAccessToken = json.data.userAccessToken;
-                const lecturerAccessToken = json.data.lecturerAccessToken;
-
-                const tokenName = user
-                  ? 'userAccessToken'
-                  : 'lecturerAccessToken';
-
-                const accessToken = user
-                  ? userAccessToken
-                  : lecturerAccessToken;
-
-                const response = NextResponse.redirect(request.url);
-
-                setCookie(response, tokenName, accessToken);
-                setCookie(response, 'refreshToken', resRefreshToken);
-
-                return response;
-              });
-            })
-            .catch(() => {
-              const includes = LOGIN_REQUIRED_URLS.includes(
-                request.nextUrl.pathname,
-              );
-
-              const response = includes
-                ? NextResponse.redirect(new URL('/login', request.url))
-                : NextResponse.redirect(request.url);
-
-              if (!includes) response.cookies.set('reload', 'true');
-
-              response.cookies.delete('userAccessToken');
-              response.cookies.delete('lecturerAccessToken');
-              response.cookies.delete('refreshToken');
-
-              return response;
-            });
-        } else {
-          return NextResponse.redirect(new URL('/error', request.url));
-        }
-      });
-  }
-
-  if (response) {
-    response;
+    checkAccessToken(headers, point).then((test) => {
+      console.log('aaa:::', test);
+    });
   }
 
   if (LOGIN_REQUIRED_URLS.includes(request.nextUrl.pathname)) {
@@ -149,3 +60,111 @@ export const middleware = (request: NextRequest) => {
 export const config = {
   matcher: ['/my/:path*', '/', '/aaa', '/instructor/apply', '/class/create'], // '/login', '/api/my/:path*', '/api/auth/logout
 };
+
+// .then((response) => {
+//   console.log('hi');
+//   return response.json();
+// })
+// .then((data) => {
+//   console.log(data);
+//   if (data.statusCode === 200) {
+//     if (user && USER_NO_ACCESS.includes(request.nextUrl.pathname)) {
+//       // 유저가 가면 안되는 lecturer 링크
+//       console.log('user');
+//       clientResponse = NextResponse.redirect(new URL('/', request.url));
+//     } else if (
+//       lecturer &&
+//       LECTURER_NO_ACCESS.includes(request.nextUrl.pathname)
+//     ) {
+//       // 강사가 가면 안되는 user 링크 확인
+//       clientResponse = NextResponse.redirect(new URL('/', request.url));
+//     } else if (
+//       NON_ACCESSIBLE_AFTER_LOGIN.includes(request.nextUrl.pathname)
+//     ) {
+//       //로그인해서 가면 안되는 링크
+//       clientResponse = NextResponse.redirect(new URL('/', request.url));
+//     } else {
+//       console.log('next');
+
+//       clientResponse = NextResponse.next();
+//     }
+//   } else if (data.statusCode === 401) {
+//     const refreshToken = request.cookies.get('refreshToken')?.value;
+
+//     return fetch(new URL(`${END_POINT}/auth/token/refresh`).href, {
+//       credentials: 'include',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Cookie: `refreshToken=${refreshToken}`,
+//       },
+//     })
+//       .then((response) => {
+//         const resCookies = response.headers
+//           .get('set-cookie')
+//           ?.split('; ');
+//         const refreshTokenCookie = resCookies?.find((cookie) =>
+//           cookie.startsWith('refreshToken='),
+//         );
+//         const resRefreshToken = refreshTokenCookie?.split('=')[1];
+//         console.log('cookie::::', resCookies);
+//         console.log('cookietoken:::', refreshTokenCookie);
+//         console.log('refreshToken:::', resRefreshToken);
+
+//         return response.json();
+//       })
+//       .then((json) => {
+//         console.log(json);
+//         if (json.statusCode === 200) {
+//           const userAccessToken = json.data.userAccessToken;
+//           const lecturerAccessToken = json.data.lecturerAccessToken;
+
+//           const tokenName = user
+//             ? 'userAccessToken'
+//             : 'lecturerAccessToken';
+
+//           const accessToken = user
+//             ? userAccessToken
+//             : lecturerAccessToken;
+
+//           clientResponse = NextResponse.redirect(request.url);
+
+//           setCookie(clientResponse, tokenName, accessToken);
+//           if (resRefreshToken) {
+//             setCookie(clientResponse, 'refreshToken', resRefreshToken);
+//           }
+//         } else {
+//           console.log('없음');
+//           const includes = LOGIN_REQUIRED_URLS.includes(
+//             request.nextUrl.pathname,
+//           );
+
+//           clientResponse = includes
+//             ? NextResponse.redirect(new URL('/login', request.url))
+//             : NextResponse.redirect(request.url);
+
+//           if (!includes) clientResponse.cookies.set('reload', 'true');
+
+//           clientResponse.cookies.delete('userAccessToken');
+//           clientResponse.cookies.delete('lecturerAccessToken');
+//           clientResponse.cookies.delete('refreshToken');
+//         }
+//       });
+//   } else {
+//     clientResponse = NextResponse.redirect(
+//       new URL('/error', request.url),
+//     );
+//   }
+// })
+// .then(() => {
+//   console.log(clientResponse);
+//   if (clientResponse) {
+//     return clientResponse;
+//   }
+
+//   if (LOGIN_REQUIRED_URLS.includes(request.nextUrl.pathname)) {
+//     //강사 & 유저 토큰이 필요한 링크(로그인이 필요한 링크)
+//     return NextResponse.redirect(new URL('/login', request.url));
+//   }
+
+//   return NextResponse.next();
+// });
