9460fd721ff95b5cab8d3eaf2acb1075b8f3b87c
hyeonjun-L <beeminor60@gmail.com>
2024-04-02 10:11:57 +0900

    feat(#345): 협의 후 결정 지도 마크 표시 구현

--- src/components/Map/AreaHighlightMap.tsx
+++ src/components/Map/AreaHighlightMap.tsx
@@ -8,27 +8,49 @@ import {
   Polyline,
   useNavermaps,
   Polygon,
+  Marker,
 } from 'react-naver-maps';
-import { WARD_LIST } from '@/constants/administrativeDistrict';
 import { searchAddressPolyline } from '@/lib/apis/searchAddress';
-import { resRegions } from '@/utils/apiDataProcessor';
 import ErrorMap from './ErrorMap';
 
 const AreaLocationMap = () => {
   const navermaps = useNavermaps();
   const regions = [
-    { region: { administrativeDistrict: '경기도', district: '전 지역' } },
+    {
+      region: { administrativeDistrict: '세종특별자치시', district: '전 지역' },
+    },
     { region: { administrativeDistrict: '부산광역시', district: '중구' } },
     { region: { administrativeDistrict: '부산광역시', district: '서구' } },
   ];
 
   const addresses = useMemo(
-    () => resRegions(regions.map(({ region }) => region)),
+    () =>
+      regions.reduce(
+        (
+          acc: { [kye: string]: string[] },
+          { region: { administrativeDistrict, district } },
+        ) => {
+          if (acc[administrativeDistrict]) {
+            acc[administrativeDistrict].push(district);
+          } else {
+            acc[administrativeDistrict] = [district];
+          }
+          return acc;
+        },
+        {},
+      ),
     [],
   );
+
+  const markerName = regions.map(({ region }) =>
+    region.district === '전 지역'
+      ? region.administrativeDistrict
+      : region.district,
+  );
+
   const getPolyline = async () => {
     const promises = Object.entries(addresses).map(([province, district]) => {
-      const isAll = WARD_LIST[province].length === district.length;
+      const isAll = district[0] === '전 지역';
       return searchAddressPolyline(
         province,
         isAll ? 'province' : 'district',
@@ -39,13 +61,16 @@ const AreaLocationMap = () => {
     const response = await Promise.all(promises);
 
     const polylineList: [number, number][][] = [];
+    const markerPoint: [number, number][] = [];
 
     response.forEach((item) => {
       if (Array.isArray(item[0][0][0])) {
         item.forEach((subItem) => {
+          markerPoint.push(subItem[0][0] as [number, number]);
           polylineList.push(...(subItem as [number, number][][]));
         });
       } else {
+        markerPoint.push(item[0][0] as [number, number]);
         polylineList.push(...(item as [number, number][][]));
       }
     });
@@ -54,14 +79,10 @@ const AreaLocationMap = () => {
       polyline.map(([x, y]) => new naver.maps.LatLng(y, x)),
     );
 
-    return convertedPolylineList;
+    return { polylineList: convertedPolylineList, markerPoint };
   };
 
-  const {
-    data: polylineList,
-    isLoading,
-    isError,
-  } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ['polyline', 'classID?'],
     queryFn: getPolyline,
     retry: false,
@@ -69,7 +90,7 @@ const AreaLocationMap = () => {
 
   return isLoading ? (
     <div className="size-full animate-pulse bg-gray-700" />
-  ) : isError || !polylineList ? (
+  ) : isError || !data ? (
     <ErrorMap />
   ) : (
     <MapDiv
@@ -79,7 +100,16 @@ const AreaLocationMap = () => {
       }}
     >
       <NaverMap zoom={7} center={new naver.maps.LatLng(36.4203004, 128.31796)}>
-        {polylineList.map((paths, index) => (
+        {data.markerPoint.map(([x, y], index) => (
+          <Marker
+            key={x}
+            position={new naver.maps.LatLng(y, x)}
+            icon={{
+              content: `<div class="markerBox">${markerName[index]}</div>`,
+            }}
+          />
+        ))}
+        {data.polylineList.map((paths, index) => (
           <Fragment key={index}>
             <Polyline
               strokeColor="pink"
