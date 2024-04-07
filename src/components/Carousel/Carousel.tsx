568fa95eff41e2d5f62f53842a464b36c000f8aa
hyeonjun-L <beeminor60@gmail.com>
2024-04-07 19:06:52 +0900

    feat(#346): 캐러셀 컴포넌트 최적화 및 리팩토링

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,178 +1,88 @@
-'use client';
 import Image from 'next/image';
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
+import {
   useState,
+  Children,
+  useMemo,
+  useEffect,
+  useRef,
+  useCallback,
 } from 'react';
-import { Arrow } from '@/../public/icons/svg';
+import { Arrow } from '@/icons/svg';
 import { Props } from '@/types/cariusel';
 
-/**
- * CarouselProps Interface
- *
- * 사용법 1
- * <relative overflow-hidden 컨테이너>
- *  <이미지 크기 제어 컨테이너>
- *    <Carousel/>
- *  </이미지 크기 제어 컨테이너>
- * </ relative overflow-hidden 컨테이너>
- *
- * 사용법 2
- * <이미지 크기 제어 및 relative overflow-hidden>
- *   <Carousel/>
- * </ 이미지 크기 제어 및 relative overflow-hidden>
- *
- * 사용법 3
- * <relative overflow-hidden 컨테이너>
- *  <이미지 크기 제어 컨테이너>
- *    <Carousel>
- *      ...children 요소
- *    </Carousel>
- *  </이미지 크기 제어 컨테이너>
- * </ relative overflow-hidden 컨테이너>
- *
- * @property {string[]} imgURL - 표시할 이미지들의 URL들이 담긴 배열, children 우선 렌더
- * @property {React.ReactNode} children - 표시할 요소들, imgURL 보다 우선순위 높음
- * @property {boolean} move - 캐러셀 움직임 활성화
- * @property {boolean} [arrow=true] - 탐색을 위해 화살표를 표시해야 하는지 여부를 나타내는 선택적 플래그 (기본값 = true)
- * @property {boolean} [priority=1] - 해당 숫자 만큼 요소를 미리 렌더 (기본값 = 1)
- * @property {boolean} [showCurrentElement =true] - 현재 캐러셀 위치 표시의 상태창 표시 여부를 나타내는 선택적 플래그 (기본값 = true)
- * @property {boolean} [showCurrentElementBackGround =true] - 상태창 표시 배경 여부를 나타내는 선택적 플래그 (기본값 = true)
- * @property {number} [gap=0] - 캐러셀 요소 사이의 간격을 rem으로 지정하는 선택적 숫자 (기본값 = 0)
- * @property {number} [carouselMoveIntervalTime = 2000] - 캐러셀 움직이는 시간을 ms로 지정하는 선택적 숫자 (기본값 = 2000ms)
- * @property {number} [arrowPushMoveWaitTime = 2000] - Arrow를 누른 후 캐러셀 움직임을 멈추는 시간을 ms로 지정하는 선택적 숫자 (기본값 = 2000ms)
- * @property {boolean} [movePause = false] - 캐러셀의 움직임을 true 동안 일시정지 (기본값 = false)
- * @property {boolean} [gotoIndex = null] - 캐러셀의 인덱스를 변경 하는 선택적 플래그 (number)
- */
-
 interface CarouselProps extends Props {
-  touchDistanceX?: number;
+  currentIndex: number;
+  touchDistanceX: number;
+  changeCurrentIndex: (index: number) => void;
 }
 
 const Carousel = ({
   imgURL,
-  move,
-  arrow = true,
-  priority = 1,
-  showCurrentElement = true,
-  gap = 0,
   children,
-  showCurrentElementBackGround = true,
+  currentIndex,
+  changeCurrentIndex,
+  priority = 1,
+  move,
+  movePause,
+  gap = 0,
   carouselMoveIntervalTime = 2000,
-  arrowPushMoveWaitTime = 2000,
-  movePause = false,
   gotoIndex,
   touchDistanceX,
+  arrow = true,
+  showCurrentElement = true,
+  showCurrentElementBackGround = true,
 }: CarouselProps) => {
-  const childrenArray = React.Children.toArray(children);
-
-  const extendForCarousel = useCallback(
-    (elementArr: React.ReactNode[] | string[]) => {
-      if (elementArr.length > 1) {
-        const newElementArr = [...elementArr, elementArr[0]];
-        newElementArr.shift();
-        return [
-          newElementArr.at(-1),
-          ...newElementArr,
-          ...newElementArr.slice(0, priority - 1),
-        ];
-      }
-      return [...elementArr];
-    },
-    [priority],
-  );
-
-  const carouselElements = useMemo(
-    () =>
-      extendForCarousel(children ? [...childrenArray] : [...(imgURL || [])]),
-    [childrenArray, imgURL],
-  );
-
-  const carouselLength = carouselElements.length;
-  const originalElements = children ? childrenArray : [...(imgURL || [])];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [loadedElementCount, setLoadedElementCount] = useState(
-    priority > 1 ? priority : 1,
-  );
   const [isAnimating, setIsAnimating] = useState(true);
   const intervalIdRef = useRef<NodeJS.Timeout | null>(null);
-  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null);
+  const currentIndexRef = useRef(currentIndex);
 
-  if (move && loadedElementCount < carouselLength) {
-    setLoadedElementCount(carouselLength);
-  }
+  const childrenArray = Children.toArray(children);
 
-  const updateImageIndex = () => {
-    setCurrentIndex((prev) => {
-      if (prev === carouselLength - priority) {
-        setIsAnimating(false);
-        return 0;
-      } else if (prev === 0) {
-        setIsAnimating(true);
-        return prev + 1;
-      } else {
-        return prev + 1;
-      }
-    });
-  };
+  const originalElements = useMemo(
+    () => (children ? [...childrenArray] : [...(imgURL || [])]),
+    [children, childrenArray, imgURL],
+  );
 
-  useEffect(() => {
-    if (movePause && intervalIdRef.current) {
-      clearInterval(intervalIdRef.current);
-    } else {
-      if (move && carouselLength > 1) {
-        setIsAnimating(true);
-        if (priority === 1) {
-          timeoutIdRef.current = setTimeout(() => updateImageIndex(), 100);
-        }
-
-        intervalIdRef.current = setInterval(
-          updateImageIndex,
-          carouselMoveIntervalTime,
-        );
-      } else {
-        setIsAnimating(false);
-        setCurrentIndex(0);
-        setTimeout(() => setIsAnimating(true), 100);
-        if (intervalIdRef.current) clearInterval(intervalIdRef.current);
-      }
+  const carouselElements = useMemo(() => {
+    if (originalElements.length > 1) {
+      const newElementArr = [...originalElements, originalElements[0]];
+      newElementArr.shift();
+      return [
+        newElementArr.at(-1),
+        ...newElementArr,
+        ...newElementArr.slice(0, priority - 1),
+      ];
     }
+    return [...originalElements];
+  }, [originalElements, priority]);
 
-    return () => {
-      if (intervalIdRef.current) clearInterval(intervalIdRef.current);
-      if (timeoutIdRef.current) clearTimeout(timeoutIdRef.current);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [move, loadedElementCount, movePause]);
+  const carouselLength = carouselElements.length;
 
-  useEffect(() => {
-    if (gotoIndex === undefined) return;
-    changeCarouselIndex(gotoIndex);
-  }, [gotoIndex]);
+  const loadedElementCount = useMemo(() => {
+    const loadedCount = priority > 1 ? priority : 1;
+    return move && loadedCount < carouselLength ? carouselLength : loadedCount;
+  }, [carouselLength, move, priority]);
 
-  const changeCarouselIndex = (index: number) => {
-    if (intervalIdRef.current) clearInterval(intervalIdRef.current);
-    if (timeoutIdRef.current) clearTimeout(timeoutIdRef.current);
+  const changeCarouselIndexHandler = useCallback(
+    (index: number) => {
+      let newIndex = index;
+      const lastIndex = originalElements.length;
+      const currentIndex = currentIndexRef.current;
 
-    setIsAnimating(false);
-
-    setCurrentIndex(index);
-
-    timeoutIdRef.current = setTimeout(() => {
-      setIsAnimating(true);
-
-      if (move) {
-        intervalIdRef.current = setInterval(
-          updateImageIndex,
-          carouselMoveIntervalTime,
-        );
+      if (index < 0) {
+        newIndex = lastIndex;
+      } else if (index > lastIndex) {
+        newIndex = 0;
       }
-    }, arrowPushMoveWaitTime);
-  };
+
+      currentIndexRef.current = newIndex;
+
+      changeCurrentIndex(newIndex);
+
+      setIsAnimating(Math.abs(currentIndex - newIndex) === 1);
+    },
+    [changeCurrentIndex, originalElements.length],
+  );
 
   const changeImage = (
     event: React.MouseEvent,
@@ -190,23 +100,37 @@ const Carousel = ({
         ? carouselLength - priority
         : currentIndex - 1;
 
-    changeCarouselIndex(index);
+    changeCarouselIndexHandler(index);
   };
+
+  useEffect(() => {
+    if (!move || movePause) {
+      if (intervalIdRef.current) clearInterval(intervalIdRef.current);
+      return;
+    }
+
+    intervalIdRef.current = setInterval(() => {
+      changeCarouselIndexHandler(currentIndexRef.current + 1);
+    }, carouselMoveIntervalTime);
+
+    return () => {
+      if (intervalIdRef.current) clearInterval(intervalIdRef.current);
+    };
+  }, [carouselMoveIntervalTime, changeCarouselIndexHandler, move, movePause]);
+
+  useEffect(() => {
+    if (gotoIndex === undefined) return;
+    changeCarouselIndexHandler(gotoIndex);
+  }, [gotoIndex]);
 
   return (
     <>
       <ul
         className={`flex h-full ${
-          !touchDistanceX &&
-          isAnimating &&
-          'transition-transform duration-[1600ms] ease-out'
+          isAnimating && 'transition-transform duration-[1600ms] ease-out'
         }`}
         style={{
-          transform: touchDistanceX
-            ? `translateX(${touchDistanceX}px)`
-            : `translateX(calc(-${100 * currentIndex}% - ${
-                gap * currentIndex
-              }rem)`,
+          transform: `translateX(${touchDistanceX + gap}px)`,
         }}
       >
         {carouselElements.slice(0, loadedElementCount).map((element, index) => (
@@ -250,7 +174,6 @@ const Carousel = ({
           ))}
         </div>
       )}
-      {/* 이놈 다이나믹으로 처리 하자 */}
       {arrow && carouselLength > 1 && (
         <>
           <Arrow
