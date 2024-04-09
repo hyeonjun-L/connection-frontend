4c0954be77d84f4b5355d27da4c0939ef6873191
hyeonjun-L <beeminor60@gmail.com>
2024-04-09 18:30:07 +0900

    refactor(#346): 유저 패스권 시멘틱 태그 전환 및 리팩토링

--- src/components/Pass/UserPass.tsx
+++ src/components/Pass/UserPass.tsx
@@ -10,40 +10,60 @@ import { userPass } from '@/types/pass';
 const UserPass = ({ passInfo }: { passInfo: userPass }) => {
   const router = useRouter();
 
+  const {
+    lecturerId,
+    id,
+    maxUsageCount,
+    price,
+    title,
+    availableMonths,
+    appliedList,
+    profileCardImageUrl,
+    nickname,
+  } = passInfo;
+
+  const moveInstructorPage = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.nativeEvent.preventDefault();
+    router.push(`/instructor/${lecturerId}`);
+  };
+
   return (
-    <dl
-      onClick={() => router.push(`/order/pass/${passInfo.id}`)}
-      className="cursor-pointer rounded-md text-sm shadow-horizontal"
+    <Link
+      href={`/order/pass/${id}`}
+      className="block rounded-md text-sm shadow-horizontal"
     >
       <div className="flex flex-col gap-1.5 p-3">
         <div className="mb-2 flex justify-between text-xl font-semibold">
           <dd className="flex w-1/2">
-            <p className="truncate">{passInfo.maxUsageCount}</p>회
+            <p className="truncate">{maxUsageCount}</p>회
           </dd>
           <dd className="flex w-1/2 flex-row-reverse">
-            원<p className="truncate">{passInfo.price.toLocaleString()}</p>
+            원<p className="truncate">{price.toLocaleString()}</p>
           </dd>
         </div>
-        <dt className="truncate">{passInfo.title}</dt>
-        <dd>이용기간 {passInfo.availableMonths}개월</dd>
+        <dt className="truncate">{title}</dt>
+        <dd>이용기간 {availableMonths}개월</dd>
         <div className="group w-fit">
-          <AppliedList appliedList={passInfo.appliedList} />
+          <AppliedList appliedList={appliedList} />
         </div>
       </div>
       <hr />
       <div className="flex items-center justify-between p-3 [&>*:nth-child(2)]:w-20">
-        <Link href={`/instructor/${passInfo.lecturerId}`} className="group">
-          <ProfileImg
-            size="xsmall"
-            src={passInfo.profileCardImageUrl}
-            nickname={passInfo.nickname}
-          />
-        </Link>
-        <Button onClick={() => router.push(`/order/pass/${passInfo.id}`)}>
-          구매하기
-        </Button>
+        {nickname ? (
+          <button onClick={moveInstructorPage} className="group">
+            <ProfileImg
+              size="xsmall"
+              src={profileCardImageUrl}
+              nickname={nickname}
+            />
+          </button>
+        ) : (
+          <div />
+        )}
+        <Button>구매하기</Button>
       </div>
-    </dl>
+    </Link>
   );
 };
 
