6d821806c75e096c72e558a58caa529373faff2a
hyeonjun-L <beeminor60@gmail.com>
2024-04-19 00:31:05 +0900

    feat/refactor(#354): queryClient defaultOptions / Mutations access토큰 재발급 후 재시도 진행하도록 구현 / 코드 간략화

--- src/lib/provider/providers.tsx
+++ src/lib/provider/providers.tsx
@@ -13,24 +13,26 @@ import { FetchError } from '@/types/types';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => {
-    const handleFetchError = async (error: Error) => {
-      if (error instanceof Error) {
-        const fetchError = error as FetchError;
-        switch (fetchError.status) {
-          case 401:
-            try {
-              await accessTokenReissuance();
-              queryClient.invalidateQueries();
-            } catch (error) {
-              reloadToast('로그인 후 이용할 수 있습니다.', 'error');
-              console.error(error);
-            }
-            break;
-          default:
-            toast.error('잠시후 다시 시도해주세요.');
-            console.error(error);
-            break;
-        }
+    const accessTokenReissuanceWrapper = (message: string) =>
+      accessTokenReissuance().catch((error) => {
+        reloadToast(message, 'error');
+        console.error(error);
+      });
+
+    const queryCacheError = async (error: Error) => {
+      if (!(error instanceof Error)) return;
+
+      const fetchError = error as FetchError;
+      switch (fetchError.status) {
+        case 401:
+          await accessTokenReissuanceWrapper(
+            '세션이 만료 되었습니다. 다시 로그인 해주세요.',
+          );
+          queryClient.invalidateQueries();
+          break;
+        default:
+          console.error(error);
+          break;
       }
     };
 
@@ -40,11 +42,24 @@ export default function Providers({ children }: { children: React.ReactNode }) {
           retry: 0,
         },
         mutations: {
-          onError: handleFetchError,
+          onError: (error: Error) => {
+            toast.error('잠시후 다시 시도해주세요.');
+            console.error(error);
+          },
+          retry: (failureCount, error) => {
+            if (error instanceof Error) {
+              const fetchError = error as FetchError;
+              if (fetchError.status === 401) {
+                accessTokenReissuanceWrapper('로그인 후 이용할 수 있습니다.');
+                return true;
+              }
+            }
+            return false;
+          },
         },
       },
       queryCache: new QueryCache({
-        onError: handleFetchError,
+        onError: queryCacheError,
       }),
     });
 
