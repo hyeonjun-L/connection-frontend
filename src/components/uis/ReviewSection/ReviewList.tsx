880dbfa508f98f7c611958bcf0300849d13dff63
hyeonjun-L <beeminor60@gmail.com>
2024-04-19 19:49:21 +0900

    refactor(#354): 클래스, 강사 reviewSection 로딩 이전 아이템 로딩 처리 안되게 수정 및 로딩 로직 간략화

--- src/components/uis/ReviewSection/ReviewList.tsx
+++ src/components/uis/ReviewSection/ReviewList.tsx
@@ -1,13 +1,13 @@
 'use client';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import { REVIEW_SECTION_TAKE } from '@/constants/constants';
 import { ArrowUpSVG, StarSVG } from '@/icons/svg';
 import { getReviews } from '@/lib/apis/reviewApis';
 import { formatShortDate } from '@/utils/dateTimeUtils';
 import SortDropdown from '@/components/Dropdown/SortDropdown';
 import { Review, UserReview } from '@/components/Review';
-import Spinner from '@/components/Spinner/Spinner';
+import ReviewLoading from '@/components/Review/ReviewLoading';
 import { IReviewResponse, ReviewOrderType } from '@/types/review';
 import { PageNavigationState } from '@/types/types';
 
@@ -45,7 +45,7 @@ const ReviewList = ({
     });
   };
 
-  const { data, isFetching, hasNextPage, fetchNextPage, isFetchingNextPage } =
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
     useInfiniteQuery({
       queryKey: ['reviews', type, targetId, orderBy],
       queryFn: fetchReviews,
@@ -76,17 +76,19 @@ const ReviewList = ({
       <div className="mb-4 flex w-full items-center justify-between">
         <h2 className="flex items-center scroll-smooth text-lg font-bold">
           {type === 'lectures' ? '클래스' : '강사'} 후기
-          {data?.pages[0].totalItemCount || 0}건
+          {data.pages[0].totalItemCount}건
           <div className="ml-3 hidden md:block">
-            <Review average={data?.pages[0].totalStars ?? 0} />
+            <Review average={data.pages[0].totalStars} />
           </div>
           <StarSVG
             width="15"
             height="14"
-            className="ml-3 fill-sub-color1 sm:block md:hidden"
+            className={`${
+              data ? 'fill-sub-color1' : 'animate-pulse fill-gray-700'
+            } ml-3 sm:block md:hidden`}
           />
           <span className="ml-1 text-gray-500">
-            ({data?.pages[0].totalStars})
+            ({data.pages[0].totalStars})
           </span>
         </h2>
 
@@ -110,43 +112,38 @@ const ReviewList = ({
       </div>
 
       <div className="mb-3.5 flex min-h-20 flex-col gap-6">
-        {isFetching || !data ? (
-          <div className="flex h-20 w-full items-center justify-center">
-            <Spinner />
-          </div>
-        ) : (
-          data.pages.map((page, i) => (
-            <Fragment key={i}>
-              {page.reviews.map((review) => (
-                <UserReview
-                  key={review.id}
-                  reviewId={review.id}
-                  src={review.user.profileImage}
-                  nickname={review.user.nickname}
-                  average={review.stars}
-                  content={review.description}
-                  date={formatShortDate(review.startDateTime)}
-                  title={review.lectureTitle}
-                  isLike={review.isLike}
-                  count={review.likeCount}
-                  link={`/report?lectureReviewId=${review.id}`}
-                  userId={review.userId}
-                />
-              ))}
-            </Fragment>
-          ))
+        {data.pages.map((page) =>
+          page.reviews.map((review) => (
+            <UserReview
+              key={review.id}
+              reviewId={review.id}
+              src={review.user.profileImage}
+              nickname={review.user.nickname}
+              average={review.stars}
+              content={review.description}
+              date={formatShortDate(review.startDateTime)}
+              title={review.lectureTitle}
+              isLike={review.isLike}
+              count={review.likeCount}
+              link={`/report?lectureReviewId=${review.id}`}
+              userId={review.userId}
+            />
+          )),
         )}
+        {isFetchingNextPage && <ReviewLoading />}
       </div>
 
-      <button
-        onClick={() => fetchNextPage()}
-        className="mx-auto flex gap-x-1.5 text-gray-500"
-      >
-        더보기
-        <div className="flex h-6 w-6 items-center justify-center rounded-full border border-solid border-gray-500 pb-0.5 text-xl font-semibold text-sub-color1">
-          +
-        </div>
-      </button>
+      {hasNextPage && (
+        <button
+          onClick={() => fetchNextPage()}
+          className="mx-auto flex gap-x-1.5 text-gray-500"
+        >
+          더보기
+          <div className="flex h-6 w-6 items-center justify-center rounded-full border border-solid border-gray-500 pb-0.5 text-xl font-semibold text-sub-color1">
+            +
+          </div>
+        </button>
+      )}
     </section>
   );
 };
