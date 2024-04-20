2b8fc7ddee7fc94defd6ca2930288491ff43331a
hyeonjun-L <beeminor60@gmail.com>
2024-04-21 00:25:37 +0900

    feat(#361): 알림 퍼블리싱 구현 완료

--- src/app/notifications/_components/NotificationItem.tsx
+++ src/app/notifications/_components/NotificationItem.tsx
@@ -1,5 +1,24 @@
+import { TrashcanSVG } from '@/icons/svg';
+
 const NotificationItem = () => {
-  return <div />;
+  return (
+    <li className="w-full rounded-md bg-white px-4 py-3 shadow-float">
+      <dl>
+        <div className="mb-2 flex w-full items-center justify-between gap-3">
+          <div className="grid grid-cols-[auto_auto] gap-x-3 text-sm">
+            <dt className="truncate font-semibold">제목제</dt>
+            <dd className="text-gray-300">7시간 전</dd>
+          </div>
+          <button className="group flex size-7 flex-shrink-0 items-center justify-center rounded-full shadow-vertical">
+            <TrashcanSVG className="size-5 stroke-gray-300 stroke-2 group-hover:stroke-sub-color1" />
+          </button>
+        </div>
+        <dd>
+          내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+        </dd>
+      </dl>
+    </li>
+  );
 };
 
 export default NotificationItem;
--- src/app/notifications/_components/NotificationList.tsx
+++ src/app/notifications/_components/NotificationList.tsx
@@ -1,7 +1,14 @@
+import NotificationItem from './NotificationItem';
+
 const NotificationList = () => {
   return (
-    <section className="flex flex-col bg-sub-color1-transparent">
-      <div />
+    <section className="flex flex-col bg-sub-color1-transparent px-4 pb-12 pt-3">
+      <ul className="flex flex-col gap-3.5">
+        <NotificationItem />
+        <NotificationItem />
+        <NotificationItem />
+        <NotificationItem />
+      </ul>
     </section>
   );
 };
